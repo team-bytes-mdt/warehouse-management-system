51a4370b611fd24104c4a7db81b0389f3b346215
Allen Mwandunga <allenmcndounger@gmail.com>
2024-11-13 12:48:27 +0100

    initial commit

---
+++ src/main/java/de/fhdo/warehouseMgmtSys/WarehouseMgmtSysApplication.java
@@ -0,0 +1,13 @@
+package de.fhdo.warehouseMgmtSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseMgmtSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseMgmtSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fhdo/warehouseMgmtSys/WarehouseMgmtSysApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhdo.warehouseMgmtSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseMgmtSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
