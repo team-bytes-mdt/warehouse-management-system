4dbea4a14adf4cd48704fe0bf78c55800d8a173a
Allen Mwandunga <allenmcndounger@gmail.com>
2024-12-06 10:09:36 +0100

    refactoring to repositories package (Persistance Layer)

--- src/main/java/de/fhdo/warehouseMgmtSys/repositories/InventoryRepository.java
+++ src/main/java/de/fhdo/warehouseMgmtSys/repositories/InventoryRepository.java
@@ -1,6 +1,6 @@
-package de.fhdo.warehouseMgmtSys.data;
+package de.fhdo.warehouseMgmtSys.repositories;
 
-import de.fhdo.warehouseMgmtSys.models.Inventory;
+import de.fhdo.warehouseMgmtSys.domain.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
