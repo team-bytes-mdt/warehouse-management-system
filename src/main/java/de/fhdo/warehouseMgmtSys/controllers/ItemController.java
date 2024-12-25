c21ac8808095e4055b07306356534e0903f386ba
Allen Mwandunga <allenmcndounger@gmail.com>
2024-12-25 20:25:24 +0100

    updating Item Controller

---
+++ src/main/java/de/fhdo/warehouseMgmtSys/controllers/ItemController.java
@@ -0,0 +1,84 @@
+package de.fhdo.warehouseMgmtSys.controllers;
+
+import de.fhdo.warehouseMgmtSys.converters.ItemConverter;
+import de.fhdo.warehouseMgmtSys.domain.Item;
+import de.fhdo.warehouseMgmtSys.dto.ItemDto;
+import de.fhdo.warehouseMgmtSys.service.ItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+
+    public ItemController(ItemService itemService){
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public String getAllItems(Model model) {
+        // Fetch all Items and convert them to DTOs
+        List<ItemDto> items = itemService.getAllItems()
+                .stream()
+                .map(ItemConverter::toDto)
+                .collect(Collectors.toList());
+
+        // Add DTO to the model
+        model.addAttribute("items", items);
+        return "items";
+    }
+
+    @GetMapping("/{id}")
+    public String getItemById(Model model, @PathVariable long id) {
+        // Fetch item and convert it to DTO
+        ItemDto item = itemService.getItemById(id)
+                .map(ItemConverter::toDto)
+                .orElse(null);
+
+        // Add DTO to the model
+        model.addAttribute("item", item);
+        return "item";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("item", new ItemDto());
+        return "create-item";
+    }
+
+    @PostMapping
+    public String createItem(@ModelAttribute ItemDto itemDto) {
+        Item item = ItemConverter.toEntity(itemDto);
+        itemService.createItem(item);
+        return "redirect:/items";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditForm(Model model, @PathVariable long id) {
+        ItemDto item = itemService.getItemById(id)
+                .map(ItemConverter::toDto)
+                .orElse(null);
+        model.addAttribute("item", item);
+        return "edit-item";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateItem(@PathVariable long id, @ModelAttribute ItemDto itemDto) {
+        Item item = ItemConverter.toEntity(itemDto);
+        itemService.updateItemById(id, item);
+        return "redirect:/items";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteItem(@PathVariable long id) {
+        itemService.deleteItemById(id);
+        return "redirect:/items";
+    }
+}
