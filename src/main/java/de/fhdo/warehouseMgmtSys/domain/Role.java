03f9230a11a0fa033d90bad42fc80a329c06ffa9
Allen Mwandunga <allenmcndounger@gmail.com>
2024-12-28 15:53:08 +0100

    update Role enum to match with DB

--- src/main/java/de/fhdo/warehouseMgmtSys/domain/Role.java
+++ src/main/java/de/fhdo/warehouseMgmtSys/domain/Role.java
@@ -1,16 +1,22 @@
 package de.fhdo.warehouseMgmtSys.domain;
 
 public enum Role {
-    ADMIN, WAREHOUSE_MANAGER, WAREHOUSE_STAFF;
+    ADMIN, WAREHOUSE_MANAGER, WAREHOUSE_STAFF, INVENTORY_MANAGER, SYSTEM_ADMINISTRATOR, CUSTOMER;
 
     public String toString() {
         switch (this) {
             case ADMIN:
-                return "Admin";
+                return "ADMIN";
             case WAREHOUSE_MANAGER:
-                return "Warehouse Manager";
+                return "WAREHOUSE MANAGER";
             case WAREHOUSE_STAFF:
-                return "Warehouse Staff";
+                return "WAREHOUSE STAFF";
+            case INVENTORY_MANAGER:
+                return "INVENTORY MANAGER";
+            case SYSTEM_ADMINISTRATOR:
+                return "SYSTEM ADMINISTRATOR";
+            case CUSTOMER:
+                return "CUSTOMER";
         }
         return "";
     }
